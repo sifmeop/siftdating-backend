dcf11b471d8c15c377bdf5d4ea1d79f0cb78cca5
Eugene <sifmeop@gmail.com>
2024-09-04 23:26:01 +0300

    comment pusher gateway

--- src/pusher/pusher.module.ts
+++ src/pusher/pusher.module.ts
@@ -1,12 +1,11 @@
 import { Global, Module } from '@nestjs/common'
 import { PusherController } from './pusher.controller'
-import { PusherGateway } from './pusher.gateway'
 import { PusherService } from './pusher.service'
 
 @Global()
 @Module({
   controllers: [PusherController],
-  providers: [PusherService, PusherGateway],
+  providers: [PusherService],
   exports: [PusherService]
 })
 export class PusherModule {}
--- src/pusher/pusher.service.ts
+++ src/pusher/pusher.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@nestjs/common'
-import { PusherGateway } from './pusher.gateway'
+// import { PusherGateway } from './pusher.gateway'
 
 @Injectable()
 export class PusherService {
-  constructor(private readonly pusherGateway: PusherGateway) {}
+  // constructor(private readonly pusherGateway: PusherGateway) {}
 
   async sendMessage(chatId: string, data: any): Promise<any> {
-    await this.pusherGateway.triggerEvent(`chat-${chatId}`, 'message', data)
+    // await this.pusherGateway.triggerEvent(`chat-${chatId}`, 'message', data)
   }
 
   async checkAuth(): Promise<any> {}
